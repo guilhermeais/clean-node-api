c637169a0d7feae0dc1bd81e3ed98f26d91bac12
Guilhermeais <guilhermeteixeiraais@gmail.com>
2022-01-27 23:24:13 -0300

    move SUT creation to a factory helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,10 +1,13 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
+const makeSut = () => {
+  return new LoginRouter()
+}
 describe('Login Router', () => {
   test('should return 400 if no email is provided', () => {
     // sut, acronym for system under test
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         password: 'anyPass'
@@ -16,7 +19,7 @@ describe('Login Router', () => {
   })
   test('should return 400 if no password is provided', () => {
     // sut, acronym for system under test
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'someemail'
@@ -29,16 +32,24 @@ describe('Login Router', () => {
 
   test('should return 500 if no httpRequest provided', () => {
     // sut, acronym for system under test
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
-  test('should return 500 if no httpRequest has no body', () => {
+  test('should return 500 if httpRequest has no body', () => {
     // sut, acronym for system under test
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
+
+  // test('should call AuthUseCase with correct params', () => {
+  //   // sut, acronym for system under test
+  //   const sut = makeSut()
+  //   const httpRequest = {}
+  //   const httpResponse = sut.route(httpRequest)
+  //   expect(httpResponse.statusCode).toBe(500)
+  // })
 })
